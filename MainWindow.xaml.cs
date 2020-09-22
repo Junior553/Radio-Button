84d85e31526994100138d0eb8ad24ef97c722aa1
Junior553 <2022pbode@laplata.k12.mo.us>
2020-09-22 10:10:19 -0500

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Radio_Button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPushMe_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButtonPencil.IsChecked == true)
+            {
+                labelAnswer.Content = " Pencil, Very Reliable! ";
+                
+            }
+            else if (radioButtonPen.IsChecked == true)
+            {
+                labelAnswer.Content = " Pen, Nice Choice ";
+
+            }
+            else if (radioButtonPhone.IsChecked == true)
+            {
+                labelAnswer.Content = " Phone, Must be from the future ";
+
+            }
+            else if (radioButtonPad.IsChecked == true)
+            {
+                labelAnswer.Content = " Pad, Welcome to 2020";
+                
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
